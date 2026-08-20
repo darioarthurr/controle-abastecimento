2ba821a120d17c7fc2fd3de744b85f9a50ada160
darioarthurr <darioarthurr@gmail.com>
2026-08-20 12:45:51 -0300

    Update API_URL to use a secure endpoint

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "AKfycbwTihM1c890OXZqoxxZYduyJ8k_SXvthQHFYndOEHcCvuEJX2HcHFIFqvED1WP_2Y4",
+  API_URL: "https://script.google.com/macros/s/AKfycbwTihM1c890OXZqoxxZYduyJ8k_SXvthQHFYndOEHcCvuEJX2HcHFIFqvED1WP_2Y4/exec",
   DB_NAME: "controle-abastecimento-v1",
   DB_VERSION: 1
 };
