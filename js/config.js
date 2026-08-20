104a800725ef1a7309ce3dd823bdb4f088eadda4
darioarthurr <darioarthurr@gmail.com>
2026-08-20 12:43:25 -0300

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "",
+  API_URL: "AKfycbwTihM1c890OXZqoxxZYduyJ8k_SXvthQHFYndOEHcCvuEJX2HcHFIFqvED1WP_2Y4",
   DB_NAME: "controle-abastecimento-v1",
   DB_VERSION: 1
 };
